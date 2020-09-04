599ca9b7085135083a550023e612ebc21071b4bd
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-04 18:23:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,17 +12,17 @@ require "pry"
 # end
 
 def reverse_each_word(str)
-  # str.reverse.split.reverse.join(" ")
-  new_str = []
-  str.reverse.split().each do |word| 
-    new_str << word.reverse
-    end.join(" ")
+  str.reverse.split.reverse.join(" ")
+  # new_str = []
+  # str.reverse.split().each do |word| 
+    # new_str << word.reverse
+    # end.join(" ")
     
 end
 
 
-# def reverse_each_word(str2)
-#   str2.split.collect do |word|
-#     word.reverse!
-#     end
-# end
+def reverse_each_word(str2)
+  str2.split.collect do |word|
+    word.reverse!
+    end
+end
