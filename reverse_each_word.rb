72f727e42334af041df167039b91196d9f387c64
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-04 16:56:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 require "pry"
 
 def reverse_each_word(str)
-  new_str = ""
+  new_str = [] 
   str.split.each do |word|
     word.reverse!
-    new_str << str 
+    new_str << str[word] 
     end
   new_str.join 
 end
