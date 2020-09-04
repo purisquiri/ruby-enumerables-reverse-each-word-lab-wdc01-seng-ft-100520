0a40137b201e91ba8e160a10b510ec3ff4fd0fcc
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-04 17:41:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ str = "Hello how are you?"
 def reverse_each_word(str)
   new_str = []
   str.split.each do |word| 
-    new_str = word.reverse!
+    new_str << word.reverse!
     
     end
   new_str.join(" ")
