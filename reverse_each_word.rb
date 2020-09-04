5f30ea2b074469ba6ac1a1e329737a1b7f3474f5
Mauro Dorigo <mau.dorigo@outlook.es>
2020-09-04 16:52:26 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+require "pry"
+
+def reverse_each_word(str)
+  str.split.each do |word|
+    word.reverse!
+    end
+end
+
+def reverse_each_word(str2)
+  str2.split.collect do |word|
+    word.reverse!
+    end
+end
